c0b17e1202ab0bf03e474aa3ee697d17809e1f26
Tahifun <rene190178@gmail.com>
2026-03-17 21:36:35 +0100

    Create siteLinks.ts

---
+++ src/config/siteLinks.ts
@@ -0,0 +1,10 @@
+export const APP_URL = "https://www.clip-boost.online";
+
+/**
+ * Wenn du eine echte Desktop-Datei anbietest, ersetze die URL unten z. B. durch:
+ * https://www.clip-boost.online/downloads/clip-boost-setup.exe
+ *
+ * Wenn du aktuell nur die Web-App hast, lass APP_DOWNLOAD_URL einfach auf APP_URL
+ * oder blende den Button spaeter aus.
+ */
+export const APP_DOWNLOAD_URL = "https://www.clip-boost.online/downloads/clip-boost-setup.exe";
